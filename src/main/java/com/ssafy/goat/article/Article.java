538d6e2f3eb736e250332ab13184cbb46e0486e3
qhfhdls <79788971+DongHyun22@users.noreply.github.com>
2023-05-13 02:39:48 +0900

    :recycle: refactor: Article도메인 변경

--- src/main/java/com/ssafy/goat/article/Article.java
+++ src/main/java/com/ssafy/goat/article/Article.java
@@ -1,41 +1,51 @@
 package com.ssafy.goat.article;
 
+import com.ssafy.goat.common.domain.TimeBaseEntity;
 import com.ssafy.goat.member.Member;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
+
+// TODO: 2023-05-12 access, PROTECTED 이게 뭘까 ?
+@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-// TODO: 2023-05-12 access, PROTECTED 이게 뭘까 ? 
-public class Article {
+public class Article extends TimeBaseEntity {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "article_id")
     private Long id;
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String content;
+    @Column(nullable = false)
     private int hit;
+    @Column(nullable = false)
     private LocalDateTime createdDate;
+    @Column(nullable = false)
     private LocalDateTime lastModifiedDate;
+    @Column(nullable = false)
     private Long memberId;
 
-    private Member member;
-
-    public Article(Long id) {
-        this.id = id;
-    }
-
     @Builder
-    public Article(Long id, String title, String content, int hit, LocalDateTime createdDate, LocalDateTime lastModifiedDate, Member member) {
+    public Article(Long id, String title, String content, int hit, LocalDateTime createdDate, LocalDateTime lastModifiedDate, Long memberId) {
         this.id = id;
         this.title = title;
         this.content = content;
         this.hit = hit;
         this.createdDate = createdDate;
         this.lastModifiedDate = lastModifiedDate;
-        this.member = member;
+        this.memberId = memberId;
     }
 
     //== 비즈니스 로직 ==//
