87153d6763cf3f0b5f51bea4752cd8283858fdf8
qhfhdls <79788971+DongHyun22@users.noreply.github.com>
2023-05-13 21:26:42 +0900

    📚docs: dump.sql 테이블 명 변경

--- docs/sql/dump.sql
+++ docs/sql/dump.sql
@@ -7,7 +7,7 @@ values (1, 'ssafy1', 'ssafy1', '임우택', 'ssafy1@ssafy.com', '01011111111', '
        (4, 'ssafy4', 'ssafy4', '김동현', 'ssafy4@ssafy.com', '01033134433', '0103', '1998', 'M', '도넛', '20210101', 'CLIENT', '-1');
 
 -- notion data
-insert into notion(notion_id, title, content, top, created_by, last_modified_by)
+insert into notice(notice_id, title, content, top, created_by, last_modified_by)
 values (1, '4월 11일 (화) 시스템 점검 및 업데이트 안내', '더욱 괘적하고 안정적인 서비스 지원을 위해 점검이 진행될 예정입니다.', 1, 1, 1),
        (2, '개인정보 처리방침 개정 안내', 'Enjoy Trip의 개인정보 처리방침이 개정될 예정이니 서비스 이용 시 참고해주시기 바랍니다.', 0, 1, 1);
 
