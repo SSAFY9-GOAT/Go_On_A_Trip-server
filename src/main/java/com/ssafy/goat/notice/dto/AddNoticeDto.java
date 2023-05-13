0525274c07b1765889b0c02c93ef3f87f56825e1
leeyr0412 <leeyr0412@naver.com>
2023-05-13 22:23:29 +0900

    🎉feat: AddNoticeDto 작성

---
+++ src/main/java/com/ssafy/goat/notice/dto/AddNoticeDto.java
@@ -0,0 +1,18 @@
+package com.ssafy.goat.notice.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class AddNoticeDto {
+    private String title;
+    private String content;
+    private String top;
+
+    @Builder
+    public AddNoticeDto(String title, String content, String top) {
+        this.title = title;
+        this.content = content;
+        this.top = top;
+    }
+}
