e6ad51227dc171987f03771245f8265560bfd208
qhfhdls12 <qhfhdls12@naver.com>
2023-05-15 17:14:13 +0900

    🌊 refactor: ArticleService 코드 변경

--- src/main/java/com/ssafy/goat/article/service/ArticleService.java
+++ src/main/java/com/ssafy/goat/article/service/ArticleService.java
@@ -1,33 +1,14 @@
 package com.ssafy.goat.article.service;
 
-import com.ssafy.goat.article.dto.ArticleDetailDto;
-import com.ssafy.goat.article.dto.ArticleDto;
 import com.ssafy.goat.article.dto.ArticleListDto;
 import com.ssafy.goat.article.dto.ArticleSearch;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 
 @Transactional
 public interface ArticleService {
-    int addArticle(Long memberId, ArticleDto articleDto);
-
     @Transactional(readOnly = true)
-    ArticleDetailDto searchArticle(Long articleId);
-
-    @Transactional(readOnly = true)
-    List<ArticleListDto> searchArticles(ArticleSearch condition, int pageNum, int amount);
-
-    @Transactional(readOnly = true)
-    List<ArticleListDto> searchMyArticles(Long memberId, int pageNum, int amount);
-
-    @Transactional(readOnly = true)
-    int getTotalCount();
-
-    int editArticle(Long articleId, Long memberId, ArticleDto articleDto);
-
-    int increaseHit(Long articleId);
-
-    int removeArticle(Long articleId, Long memberId);
+    Page<ArticleListDto> searchArticles(ArticleSearch condition, Pageable pageable);
 }
--- src/main/java/com/ssafy/goat/article/service/ArticleServiceImpl.java
+++ src/main/java/com/ssafy/goat/article/service/ArticleServiceImpl.java
@@ -1,58 +1,21 @@
 package com.ssafy.goat.article.service;
 
-import com.ssafy.goat.article.dto.ArticleDetailDto;
-import com.ssafy.goat.article.dto.ArticleDto;
 import com.ssafy.goat.article.dto.ArticleListDto;
 import com.ssafy.goat.article.dto.ArticleSearch;
 import com.ssafy.goat.article.repository.ArticleRepository;
-import com.ssafy.goat.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleServiceImpl implements ArticleService{
 
+    private final ArticleRepository articleRepository;
 
     @Override
-    public int addArticle(Long memberId, ArticleDto articleDto) {
-        return 0;
-    }
-
-    @Override
-    public ArticleDetailDto searchArticle(Long articleId) {
-        return null;
-    }
-
-    @Override
-    public List<ArticleListDto> searchArticles(ArticleSearch condition, int pageNum, int amount) {
-        return null;
-    }
-
-    @Override
-    public List<ArticleListDto> searchMyArticles(Long memberId, int pageNum, int amount) {
-        return null;
-    }
-
-    @Override
-    public int getTotalCount() {
-        return 0;
-    }
-
-    @Override
-    public int editArticle(Long articleId, Long memberId, ArticleDto articleDto) {
-        return 0;
-    }
-
-    @Override
-    public int increaseHit(Long articleId) {
-        return 0;
-    }
-
-    @Override
-    public int removeArticle(Long articleId, Long memberId) {
-        return 0;
+    public Page<ArticleListDto> searchArticles(ArticleSearch condition, Pageable pageable) {
+        return articleRepository.searchByCondition(condition, pageable);
     }
 }
