b4909a0d1f32fed4b8daf74052d50966c43787c8
leeyr0412 <leeyr0412@naver.com>
2023-05-16 23:15:36 +0900

    🌊 refactor: 비밀번호 변경 리퀘스트 변경

--- src/main/java/com/ssafy/goat/controller/request/ChangPasswordRequest.java
+++ src/main/java/com/ssafy/goat/controller/request/ChangPasswordRequest.java
@@ -3,11 +3,14 @@ package com.ssafy.goat.controller.request;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
 public class ChangPasswordRequest {
+    @NotNull
+    private Long id;
     @NotBlank
     @Size(min = 8, max = 20)
     private String originalPw;
