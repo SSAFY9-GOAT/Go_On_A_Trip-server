8a75ca18daa88f97ef24d7cd1298e6334b98ce29
qhfhdls12 <qhfhdls12@naver.com>
2023-05-16 17:57:23 +0900

    🌊 refactor: ArticleDetailDto 변경

---
+++ src/main/java/com/ssafy/goat/common/exception/ArticleException.java
@@ -0,0 +1,8 @@
+package com.ssafy.goat.common.exception;
+
+public class ArticleException extends RuntimeException {
+
+    public ArticleException(String message) {
+        super(message);
+    }
+}
