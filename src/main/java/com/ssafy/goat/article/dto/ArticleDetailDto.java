94cdc4d0cfdbb01e2d9853e70cdcaab6c9ef27c2
qhfhdls12 <qhfhdls12@naver.com>
2023-05-16 17:57:55 +0900

    🌊 refactor: ArticleDetailDto 변경

--- src/main/java/com/ssafy/goat/article/dto/ArticleDetailDto.java
+++ src/main/java/com/ssafy/goat/article/dto/ArticleDetailDto.java
@@ -3,19 +3,21 @@ package com.ssafy.goat.article.dto;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class ArticleDetailDto {
 
     private Long articleId;
     private String title;
     private String content;
-    private String createdDate;
+    private LocalDateTime createdDate;
 
     private Long memberId;
     private String nickname;
 
     @Builder
-    public ArticleDetailDto(Long articleId, String title, String content, String createdDate, Long memberId, String nickname) {
+    public ArticleDetailDto(Long articleId, String title, String content, LocalDateTime createdDate, Long memberId, String nickname) {
         this.articleId = articleId;
         this.title = title;
         this.content = content;
