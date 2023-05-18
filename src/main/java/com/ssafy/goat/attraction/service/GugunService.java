4168687690a93b8634f59a5070b13ee7267c504a
qhfhdls12 <qhfhdls12@naver.com>
2023-05-18 17:54:36 +0900

    🌞 feat: AttractionService 추가

---
+++ src/main/java/com/ssafy/goat/attraction/service/GugunService.java
@@ -0,0 +1,4 @@
+package com.ssafy.goat.attraction.service;
+
+public interface GugunService {
+}
---
+++ src/main/java/com/ssafy/goat/attraction/service/GugunServiceImpl.java
@@ -0,0 +1,4 @@
+package com.ssafy.goat.attraction.service;
+
+public class GugunServiceImpl implements GugunService{
+}
