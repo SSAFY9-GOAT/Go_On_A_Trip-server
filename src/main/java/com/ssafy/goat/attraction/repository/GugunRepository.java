52315f813d4ef7ae2e3e753cf7397df3ce64ed7e
qhfhdls12 <qhfhdls12@naver.com>
2023-05-18 17:54:24 +0900

    🌞 feat: AttractionRepository 추가

---
+++ src/main/java/com/ssafy/goat/attraction/repository/GugunRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.goat.attraction.repository;
+
+import com.ssafy.goat.attraction.Gugun;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface GugunRepository extends JpaRepository<Gugun, Long> {
+}
