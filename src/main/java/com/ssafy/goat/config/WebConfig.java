091035714cfa29586915c6119fdb98dd3a9701d0
leeyr0412 <leeyr0412@naver.com>
2023-05-18 10:15:51 +0900

    🎉 feat: 로그인 인터셉터 기능 구현(CORS 해결)

--- src/main/java/com/ssafy/goat/config/WebConfig.java
+++ src/main/java/com/ssafy/goat/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.ssafy.goat.config;
 import com.ssafy.goat.config.interceptor.JwtInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -25,11 +26,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .excludePathPatterns(EXCLUDE_PATHS);
     }
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry){
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("GET", "POST")
-//                .maxAge(3000);
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000/")
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
+                        HttpMethod.PATCH.name())
+                .exposedHeaders("Authorization")
+                .maxAge(3000);
+    }
 }
