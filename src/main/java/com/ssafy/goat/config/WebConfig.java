3e0578e668b6f7366097175621463e75ea497953
qhfhdls12 <qhfhdls12@naver.com>
2023-05-18 17:52:18 +0900

    🌊 refactor: Interceptor에 attraction 추가

--- src/main/java/com/ssafy/goat/config/WebConfig.java
+++ src/main/java/com/ssafy/goat/config/WebConfig.java
@@ -12,7 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     private static final String[] EXCLUDE_PATHS = {
             "/login", "/regist", "/logout/**",
             "/error/**",
-            "/articles"
+            "/articles",
+            "/api/**"
     };
 
     @Autowired
