e6e80fdb11d65ebdda4730e15228840ec5de9ceb
qhfhdls12 <qhfhdls12@naver.com>
2023-05-18 17:54:57 +0900

    🌞 feat: SidoRepository 추가

--- src/main/java/com/ssafy/goat/attraction/dto/SidoDto.java
+++ src/main/java/com/ssafy/goat/attraction/dto/SidoDto.java
@@ -1,16 +1,16 @@
-//package com.ssafy.goat.attraction.dto;
-//
-//import lombok.Builder;
-//import lombok.Data;
-//
-//@Data
-//public class SidoDto {
-//    private Integer code;
-//    private String name;
-//
-//    @Builder
-//    public SidoDto(Integer code, String name) {
-//        this.code = code;
-//        this.name = name;
-//    }
-//}
+package com.ssafy.goat.attraction.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class SidoDto {
+    private Integer code;
+    private String name;
+
+    @Builder
+    public SidoDto(Integer code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/ssafy/goat/attraction/repository/SidoRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.goat.attraction.repository;
+
+import com.ssafy.goat.attraction.Sido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface SidoRepository extends JpaRepository<Sido, Long> {
+}
