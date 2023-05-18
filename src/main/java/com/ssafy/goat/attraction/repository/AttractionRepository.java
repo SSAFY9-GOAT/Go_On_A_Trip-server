0afa21cfeaa98281dfbee187e31394e1be228388
qhfhdls12 <qhfhdls12@naver.com>
2023-05-18 17:53:33 +0900

    🌞 feat: AttractionRepository 추가

--- src/main/java/com/ssafy/goat/attraction/repository/AttractionRepository.java
+++ src/main/java/com/ssafy/goat/attraction/repository/AttractionRepository.java
@@ -3,5 +3,5 @@ package com.ssafy.goat.attraction.repository;
 import com.ssafy.goat.attraction.AttractionInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AttractionRepository extends JpaRepository<AttractionInfo, Integer>, AttractionRepositoryCustom {
+public interface AttractionRepository extends JpaRepository<AttractionInfo, Integer> {
 }
--- src/main/java/com/ssafy/goat/attraction/repository/AttractionRepositoryCustom.java
+++ src/main/java/com/ssafy/goat/attraction/repository/AttractionRepositoryCustom.java
@@ -1,11 +1,11 @@
-package com.ssafy.goat.attraction.repository;
-
-import com.ssafy.goat.attraction.repository.dto.AttractionSearchCondition;
-import com.ssafy.goat.controller.response.AttractionResponse;
-
-import java.util.List;
-
-public interface AttractionRepositoryCustom {
-
-    List<AttractionResponse> searchByCondition(AttractionSearchCondition condition);
-}
+//package com.ssafy.goat.attraction.repository;
+//
+//import com.ssafy.goat.attraction.repository.dto.AttractionSearchCondition;
+//import com.ssafy.goat.controller.response.AttractionResponse;
+//
+//import java.util.List;
+//
+//public interface AttractionRepositoryCustom {
+//
+//    List<AttractionResponse> searchByCondition(AttractionSearchCondition condition);
+//}
