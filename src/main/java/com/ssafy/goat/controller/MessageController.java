1360e7da872e65864be1ac27e208333c2489e875
leeyr0412 <leeyr0412@naver.com>
2023-05-19 17:42:19 +0900

    🌞 feat: 비밀번호 찾아 문자 발송 컨트롤러 구현

--- src/main/java/com/ssafy/goat/controller/MessageController.java
+++ src/main/java/com/ssafy/goat/controller/MessageController.java
@@ -1,5 +1,10 @@
 package com.ssafy.goat.controller;
 
+import com.ssafy.goat.controller.request.FindPwRequest;
+import com.ssafy.goat.member.service.AccountService;
+import com.ssafy.goat.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.model.Message;
 import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
@@ -7,35 +12,90 @@ import net.nurigo.sdk.message.response.SingleMessageSentResponse;
 import net.nurigo.sdk.message.service.DefaultMessageService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.security.SecureRandom;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 @RestController
+@Slf4j
+@RequiredArgsConstructor
 public class MessageController {
-    final DefaultMessageService messageService;
+
+    private final AccountService accountService;
+    private final MemberService memberService;
+
+    private DefaultMessageService messageService;
 
     @Value("${MESSAGE_API_KEY}")
     private String API_KEY;
     @Value("${MESSAGE_API_SECRET}")
     private String API_SECRET_KEY;
+    private String targetUrl = "http://api.coolsms.co.kr/messages/v4/send";
 
-    public MessageController() {
-        this.messageService = NurigoApp.INSTANCE.initialize(API_KEY, API_SECRET_KEY, "https://api.coolsms.co.kr");
-    }
+    private static final String SUCCESS = "success";
+    private static final String FAIL = "fail";
 
     /**
-     * 단일 메시지 발송 예제
+     * 단일 메시지 발송
      */
     @PostMapping("/findPw")
-    public SingleMessageSentResponse findPw() {
-        Message message = new Message();
-        // 발신번호 및 수신번호는 반드시 01012345678 형태로 입력되어야 합니다.
-        message.setFrom("발신번호 입력");
-        message.setTo("수신번호 입력");
-        message.setText("한글 45자, 영자 90자 이하 입력되면 자동으로 SMS타입의 메시지가 추가됩니다.");
+    public Map<String, String> findPw(
+            @Valid @RequestBody FindPwRequest request
+    ) {
+        log.debug("비밀번호 찾기 요청됨 request = {}", request);
+        this.messageService = NurigoApp.INSTANCE.initialize(API_KEY, API_SECRET_KEY, "https://api.coolsms.co.kr");
+        Map<String, String> resultMap = new HashMap<>();
 
-        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
-        System.out.println(response);
+        String loginId = request.getLoginId();
+        StringTokenizer st = new StringTokenizer(request.getPhone(), "-");
+        String phone = st.nextToken() + st.nextToken() + st.nextToken();
+        log.debug("[비밀번호 찾기] 아이디 =" + loginId);
+        log.debug("[비밀번호 찾기] 전화번호 =" + phone);
+        if (accountService.getUserPhone(loginId, phone)) {
+            log.debug("입력 제대로 되어있음 - 유저 찾음");
 
-        return response;
+            String newPw = getRamdomPassword(12);
+
+            Message message = new Message();
+            message.setFrom(phone);
+            message.setTo(phone);
+            message.setText("[GOAT] 임시번호가 발급되었습니다.\n" + newPw + "\n로그인 후 비밀번호를 변경하세요.");
+            SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
+
+            Long member = memberService.findAndChangePassword(loginId, newPw);
+            resultMap.put("message", SUCCESS);
+        } else {
+            log.debug("입력 제대로 안됨 - 유저 없음");
+            resultMap.put("message", FAIL);
+        }
+        return resultMap;
+    }
+
+
+    public String getRamdomPassword(int size) {
+        char[] charSet = new char[]{
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                '!', '@', '#', '$', '%', '^', '&'};
+
+        StringBuffer sb = new StringBuffer();
+        SecureRandom sr = new SecureRandom();
+        sr.setSeed(new Date().getTime());
+
+        int idx = 0;
+        int len = charSet.length;
+        for (int i = 0; i < size; i++) {
+            idx = sr.nextInt(len);
+            sb.append(charSet[idx]);
+        }
+
+        return sb.toString();
     }
 }
