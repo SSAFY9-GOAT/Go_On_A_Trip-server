8fa3c2c9293a8977f50a4b1acdda47afa817111d
leeyr0412 <leeyr0412@naver.com>
2023-05-19 10:40:52 +0900

    🌞 feat: 메세지 전송을 위한 Cotroller 생성

--- src/main/java/com/ssafy/goat/controller/MessageController.java
+++ src/main/java/com/ssafy/goat/controller/MessageController.java
@@ -2,13 +2,19 @@ package com.ssafy.goat.controller;
 
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
     final DefaultMessageService messageService;
 
-    public MessageController(){
-        this.messageService = NurigoApp.INSTANCE.initialize()
+    @Value("${MESSAGE_API_KEY}")
+    private String API_KEY;
+    @Value("${MESSAGE_API_SECRET}")
+    private String API_SECRET_KEY;
+
+    public MessageController() {
+        this.messageService = NurigoApp.INSTANCE.initialize(API_KEY, API_SECRET_KEY, "https://api.coolsms.co.kr");
     }
 }
