f7ea8a2ae3f6e0a43fc07c773e2162580f0916e7
leeyr0412 <leeyr0412@naver.com>
2023-05-19 17:39:02 +0900

    🌊 refactor: 비밀번호 찾기 인터셉터 제외

--- src/main/java/com/ssafy/goat/config/WebConfig.java
+++ src/main/java/com/ssafy/goat/config/WebConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private static final String[] EXCLUDE_PATHS = {
-            "/login", "/regist", "/logout/**", "/user/**",
+            "/login", "/regist", "/logout/**", "/user/**", "/findPw",
             "/error/**",
             "/articles",
             "/api/**"
