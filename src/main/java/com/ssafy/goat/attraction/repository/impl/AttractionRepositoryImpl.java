9b7af414f2aedb9857c063f67cc355481dbe560e
qhfhdls12 <qhfhdls12@naver.com>
2023-05-19 15:01:09 +0900

    🌞 feat: 관광지 정보 불러오기 기능 구현

--- src/main/java/com/ssafy/goat/attraction/repository/impl/AttractionRepositoryImpl.java
+++ src/main/java/com/ssafy/goat/attraction/repository/impl/AttractionRepositoryImpl.java
@@ -2,10 +2,8 @@ package com.ssafy.goat.attraction.repository.impl;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.ssafy.goat.attraction.AttractionInfo;
 import com.ssafy.goat.attraction.dto.AttractionSearch;
 import com.ssafy.goat.attraction.repository.AttractionRepositoryCustom;
-import com.ssafy.goat.attraction.repository.dto.AttractionSearchCondition;
 import com.ssafy.goat.controller.response.AttractionResponse;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +11,9 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 import static com.ssafy.goat.attraction.QAttractionInfo.attractionInfo;
+import static com.ssafy.goat.attraction.QContentType.contentType;
+import static com.ssafy.goat.attraction.QGugun.gugun;
+import static com.ssafy.goat.attraction.QSido.sido;
 
 @Repository
 public class AttractionRepositoryImpl implements AttractionRepositoryCustom {
@@ -23,26 +24,25 @@ public class AttractionRepositoryImpl implements AttractionRepositoryCustom {
         this.queryFactory = new JPAQueryFactory(em);
     }
 
-//    @Override
-//    public List<AttractionResponse> searchByCondition(AttractionSearch condition) {
-//        return queryFactory
-//                .select(Projections.fields(AttractionResponse.class))
-//                .from(attractionInfo)
-//                .where(
-//                        attractionInfo.sido.code.eq(condition.getSidoCode()),
-//                        attractionInfo.gugun.code.eq(condition.getGugunCode())
-//                )
-//                .fetch();
-//    }
-
     @Override
-    public List<AttractionResponse> searchByCondition(AttractionSearchCondition condition) {
+    public List<AttractionResponse> searchByCondition(AttractionSearch condition) {
         return queryFactory
-                .select(Projections.fields(AttractionResponse.class))
+                .select(Projections.constructor(AttractionResponse.class,
+                        attractionInfo.id,
+                        attractionInfo.title,
+                        attractionInfo.addr1,
+                        attractionInfo.zipcode,
+                        attractionInfo.tel,
+                        attractionInfo.firstImage,
+                        attractionInfo.latitude,
+                        attractionInfo.longitude))
                 .from(attractionInfo)
-                .where(
-                        attractionInfo.sido.code.eq(condition.getSidoCode()),
-                        attractionInfo.gugun.code.eq(condition.getGugunCode())
+                .join(attractionInfo.sido, sido)
+                .join(attractionInfo.gugun, gugun)
+                .join(attractionInfo.contentType, contentType)
+                .where(attractionInfo.sido.code.eq(condition.getSidoCode())
+                        .and(attractionInfo.gugun.code.eq(condition.getGugunCode()))
+                        .and(attractionInfo.contentType.id.eq(condition.getContentTypeId()))
                 )
                 .fetch();
     }
