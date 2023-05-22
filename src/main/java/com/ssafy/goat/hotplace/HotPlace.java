b6c27098ceee7860c7f62d450e41a941ea8b0f5e
leeyr0412 <leeyr0412@naver.com>
2023-05-22 14:21:29 +0900

    🌊 refactor: 핫플레이스 도메인 변경

--- src/main/java/com/ssafy/goat/hotplace/HotPlace.java
+++ src/main/java/com/ssafy/goat/hotplace/HotPlace.java
@@ -18,34 +18,38 @@ import static javax.persistence.FetchType.LAZY;
 public class HotPlace extends TimeBaseEntity {
     @Id
     @GeneratedValue
-    @Column(name = "hot_place_id")
+    @Column(name = "`hot_place_id`")
     private Long hotPlaceId;
     @Column(nullable = false)
     private String name;
     @Lob
-    @Column(nullable = false)
+    @Column(name = "`desc`",nullable = false)
     private String desc;
+    private double latitude;
+    private double longitude;
     @Column(nullable = false)
     private int hit;
     @Column(nullable = false)
     private int vote;
     @Column(nullable = false)
-    private String visitedDazte;
+    private String visitedDate;
     @OneToOne(fetch = LAZY)
-    @JoinColumn(name = "file_id")
+    @JoinColumn(name = "`file_id`")
     private UploadFile uploadFile;
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "`member_id`")
     private Member member;
 
     @Builder
-    public HotPlace(Long hotPlaceId, String name, String desc, int hit, int vote, String visitedDazte, UploadFile uploadFile, Member member) {
+    public HotPlace(Long hotPlaceId, String name, String desc, double latitude, double longitude, int hit, int vote, String visitedDate, UploadFile uploadFile, Member member) {
         this.hotPlaceId = hotPlaceId;
         this.name = name;
         this.desc = desc;
+        this.latitude = latitude;
+        this.longitude = longitude;
         this.hit = hit;
         this.vote = vote;
-        this.visitedDazte = visitedDazte;
+        this.visitedDate = visitedDate;
         this.uploadFile = uploadFile;
         this.member = member;
     }
