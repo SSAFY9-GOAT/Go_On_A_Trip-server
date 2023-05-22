578df5c108a6bdab79875a35f49bade8b25a5dc1
leeyr0412 <leeyr0412@naver.com>
2023-05-22 14:22:42 +0900

    🌞 feat: HotplaceService 구현

--- src/main/java/com/ssafy/goat/hotplace/service/HotplaceService.java
+++ src/main/java/com/ssafy/goat/hotplace/service/HotplaceService.java
@@ -1,8 +1,9 @@
 package com.ssafy.goat.hotplace.service;
 
+import com.ssafy.goat.hotplace.service.dto.AddHotPlaceDto;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 public interface HotplaceService {
-
+    Long writeHotPlace(String loginId, AddHotPlaceDto addHotPlaceDto);
 }
