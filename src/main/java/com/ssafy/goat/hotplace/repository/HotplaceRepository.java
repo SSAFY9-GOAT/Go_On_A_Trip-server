b678ac00bfc900a2559bf2f5e317741f3d1caf76
leeyr0412 <leeyr0412@naver.com>
2023-05-22 17:49:26 +0900

    🌞 feat: HotPlace 리스트 조회 Repository 구현

--- src/main/java/com/ssafy/goat/hotplace/repository/HotplaceRepository.java
+++ src/main/java/com/ssafy/goat/hotplace/repository/HotplaceRepository.java
@@ -3,5 +3,5 @@ package com.ssafy.goat.hotplace.repository;
 import com.ssafy.goat.hotplace.HotPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HotplaceRepository extends JpaRepository<HotPlace, Long> {
+public interface HotplaceRepository extends JpaRepository<HotPlace, Long>, HotplaceRepositoryCustom {
 }
---
+++ src/main/java/com/ssafy/goat/hotplace/repository/impl/HotplaceRepositoryCustomImpl.java
@@ -0,0 +1,53 @@
+package com.ssafy.goat.hotplace.repository.impl;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.goat.controller.request.HotPlaceListRequest;
+import com.ssafy.goat.controller.response.HotPlaceListResponse;
+import com.ssafy.goat.hotplace.repository.HotplaceRepositoryCustom;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.ssafy.goat.hotplace.QHotPlace.*;
+import static com.ssafy.goat.common.domain.QUploadFile.*;
+import static com.ssafy.goat.member.QMember.member;
+
+public class HotplaceRepositoryCustomImpl implements HotplaceRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    public HotplaceRepositoryCustomImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public List<HotPlaceListResponse> searchByCondition(HotPlaceListRequest request) {
+        List<HotPlaceListResponse> list = queryFactory
+                .select(Projections.constructor(HotPlaceListResponse.class,
+                        hotPlace.hotPlaceId,
+                        hotPlace.name,
+                        hotPlace.desc,
+                        hotPlace.hit,
+                        uploadFile.storeFileName,
+                        member.nickname,
+                        hotPlace.createdDate))
+                .from(hotPlace)
+                .join(hotPlace.member, member)
+                .join(hotPlace.uploadFile, uploadFile)
+                .where(hotPlace.name.like("%" + request.getName() + "%"))
+                .orderBy(request.getSortCondition() == 1 ? hotPlace.createdDate.desc() : hotPlace.hit.desc())
+                .fetch();
+        return list;
+
+
+//        private Long hotPlaceId;
+//        private String name;
+//        private String desc;
+//        private int hit;
+//        private String storeFileName;
+//
+//        private String nickname;
+//        private String createdDate;
+    }
+}
