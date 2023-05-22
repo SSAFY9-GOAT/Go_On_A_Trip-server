95cb6f3342bdfa5c70076169e36bcfd81c8b4dbe
leeyr0412 <leeyr0412@naver.com>
2023-05-22 15:53:04 +0900

    🌞 feat: HotPlace 이미지 위치정보 반환 Controller 구현

--- src/main/java/com/ssafy/goat/controller/HotplaceController.java
+++ src/main/java/com/ssafy/goat/controller/HotplaceController.java
@@ -5,6 +5,7 @@ import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.GpsDirectory;
 import com.ssafy.goat.controller.request.AddHotplaceRequest;
+import com.ssafy.goat.controller.request.ImageRequest;
 import com.ssafy.goat.hotplace.service.HotplaceService;
 import com.ssafy.goat.hotplace.service.dto.AddHotPlaceDto;
 import io.swagger.annotations.ApiOperation;
@@ -19,6 +20,8 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -77,5 +80,43 @@ public class HotplaceController {
                 .storeFileName(storeFileName)
                 .build();
         return hotplaceService.writeHotPlace(request.getLoginId(), addHotPlaceDto);
+    }
+
+    @PostMapping("/getImageLocation")
+    @ApiOperation(value = "이미지 주소 가져오기")
+    public Map<String, Double> getImageLocation(@Valid ImageRequest request) throws ImageProcessingException, IOException {
+        MultipartFile multipartFile = request.getFile();
+
+        String originalFilename = multipartFile.getOriginalFilename();
+
+        int pos = originalFilename.lastIndexOf(".");
+
+        String ext = originalFilename.substring(pos + 1);
+        String uuid = UUID.randomUUID().toString();
+        String storeFileName = uuid + "." + ext;
+        multipartFile.transferTo(new File(getFullPath(storeFileName)));
+        log.debug("[핫플레이스] 파일 저장 완료 = {}", getFullPath(storeFileName));
+
+        log.debug("[핫플레이스] 위도 경도 추출 시작");
+        File file = new File(getFullPath(storeFileName));
+
+        Metadata metadata = ImageMetadataReader.readMetadata(file);
+        GpsDirectory gpsDirectory = metadata.getFirstDirectoryOfType(GpsDirectory.class);
+        String pdsLat = "";
+        String pdsLon = "";
+        if (gpsDirectory.containsTag(GpsDirectory.TAG_LATITUDE) && gpsDirectory.containsTag(GpsDirectory.TAG_LONGITUDE)) {
+
+            pdsLat = String.valueOf(gpsDirectory.getGeoLocation().getLatitude());
+            pdsLon = String.valueOf(gpsDirectory.getGeoLocation().getLongitude());
+        }
+        double lat = Double.parseDouble(pdsLat);    //위도
+        double lon = Double.parseDouble(pdsLon);
+
+        file.delete();
+
+        Map<String, Double> result = new HashMap<>();
+        result.put("lat", lat);
+        result.put("lon", lon);
+        return result;
     }
 }
