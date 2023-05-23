f96d994fb1e6214404448b59c1fcd29b12ef5b65
qhfhdls12 <qhfhdls12@naver.com>
2023-05-23 10:51:52 +0900

    🌊 refactor: 여행계획 삭제버튼 표시를 위한 Dto 수정

--- src/main/java/com/ssafy/goat/tripplan/dto/TripPlanDto.java
+++ src/main/java/com/ssafy/goat/tripplan/dto/TripPlanDto.java
@@ -10,11 +10,14 @@ public class TripPlanDto {
     private Long tripPlanId;
     private String title;
     private List<DetailPlanDto> detailPlans;
+    private String nickname;
 
     @Builder
-    public TripPlanDto(Long tripPlanId, String title, List<DetailPlanDto> detailPlans) {
+
+    public TripPlanDto(Long tripPlanId, String title, List<DetailPlanDto> detailPlans, String nickname) {
         this.tripPlanId = tripPlanId;
         this.title = title;
         this.detailPlans = detailPlans;
+        this.nickname = nickname;
     }
 }
