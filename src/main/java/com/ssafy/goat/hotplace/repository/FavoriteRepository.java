5c22342a9ccbd0bcd2003a60b9017f19c4742098
leeyr0412 <leeyr0412@naver.com>
2023-05-25 08:39:25 +0900

    🌞 feat: 핫플레이스 좋아요 목록 가져오기 Repository 구현

--- src/main/java/com/ssafy/goat/hotplace/repository/FavoriteRepository.java
+++ src/main/java/com/ssafy/goat/hotplace/repository/FavoriteRepository.java
@@ -1,7 +1,12 @@
 package com.ssafy.goat.hotplace.repository;
 
 import com.ssafy.goat.hotplace.Favorite;
+import com.ssafy.goat.hotplace.HotPlace;
+import com.ssafy.goat.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FavoriteRepository extends JpaRepository<Favorite, Long>, FavoriteRepositoryCustom {
+    Optional<Favorite> findByMemberAndHotPlace(Member member, HotPlace hotPlace);
 }
